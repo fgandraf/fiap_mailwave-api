7ce91c96e6805271fab6d90a426f2b09fcc7fee6
fgandraf <fgandraf@gmail.com>
2024-09-13 16:43:08 -0300

    First commit

---
+++ src/main/java/com/mailwave/api/MailWaveApiApplication.java
@@ -0,0 +1,13 @@
+package com.mailwave.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailWaveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailWaveApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mailwave/api/MailWaveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailwave.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailWaveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
